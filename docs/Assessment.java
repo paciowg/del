67ab4584ee695626a03338795c2bb3f748536435
Tim Shaffer <timshaffer@me.com>
2019-07-16 13:22:11 -0400

    Update dependencies.

--- docs/Assessment.java
+++ docs/Assessment.java
@@ -1,4 +1,4 @@
-package https://impact-fhir.mitre.org/r4/ImplementationGuide/impact-fhir;
+package https://impact-fhir.mitre.org/r4/ImplementationGuide/impact-fhir-0.0.1;
 
 import org.hl7.fhir.r5.model.ProfilingWrapper;
 
