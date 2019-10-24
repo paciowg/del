966ba2c9461a19f85c47eee3f5729e2f8bebfe55
Tim Shaffer <timshaffer@me.com>
2019-10-24 16:30:06 -0400

    Remove unused files.

--- cleanup.js
+++ cleanup.js
@@ -1,44 +1,43 @@
 const request = require('request-promise-native');
 
-const BASE_URL = 'https://api.logicahealth.org/PACIO/open/';
+const BASE_URL = 'https://impact-fhir.mitre.org/r4';
 
 async function cleanup(resource, page = 1) {
-  const response = await request({
-    method: 'GET',
-    uri: `${BASE_URL}/${resource}?_count=50`,
-    json: true,
-    resolveWithFullResponse: true
-  });
-
-  console.log(`${resource} Page ${page}`);
-
-  if (response.body.entry) {
-    for (const res of response.body.entry) {
-      console.log(res.fullUrl);
-      await request({
-        method: 'DELETE',
-        uri: res.fullUrl,
+    const response = await request({
+        method: 'GET',
+        uri: `${BASE_URL}/${resource}?_count=50`,
         json: true,
-        resolveWithFullResponse: true
-      });
+        resolveWithFullResponse: true,
+    });
+
+    console.log(`${resource} Page ${page}`);
+
+    if (response.body.entry) {
+        for (const res of response.body.entry) {
+            console.log(res.fullUrl);
+            await request({
+                method: 'DELETE',
+                uri: res.fullUrl,
+                json: true,
+                resolveWithFullResponse: true,
+            });
+        }
     }
-  }
 
-  let nextPage;
-  if (response.body.link) {
-    nextPage = response.body.link.find(link => link.relation === 'next');
-  }
+    let nextPage;
+    if (response.body.link) {
+        nextPage = response.body.link.find(link => link.relation === 'next');
+    }
 
-  if (nextPage) {
-    await cleanup(nextPage.url, ++page);
-  }
+    if (nextPage) {
+        await cleanup(nextPage.url, ++page);
+    }
 }
 
 async function main() {
-  await cleanup('Questionnaire');
-  await cleanup('Measure');
-  await cleanup('Library');
-  await cleanup('StructureDefinition');
+    await cleanup('Questionnaire');
+    await cleanup('SearchParameter');
+    await cleanup('StructureDefinition');
 }
 
 main();
